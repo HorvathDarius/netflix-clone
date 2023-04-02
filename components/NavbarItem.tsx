c660534910f571e41f777ec3e10f8350e0abd4a9
Darius Horvath <dariushorvath33@gmail.com>
2023-04-02 11:59:34 +0200

    Implemented NavBar component

---
+++ components/NavbarItem.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface navbarItemProps {
+  label: string;
+}
+
+const NavbarItem: React.FC<navbarItemProps> = ({
+    label
+}) => {
+  return (
+    <div className="text-white cursor-pointer hover:text-gray-300 transition">
+      {label}
+    </div>
+  );
+};
+
+export default NavbarItem;
